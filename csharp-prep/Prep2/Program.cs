6f3f6a0d06fd445d42ac60ac86c78b4a6fdd0387
Nr1cha <vrm.richards@gmail.com>
2023-01-06 16:07:58 -0700

    adding prep2 assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        int a = 90;
+        int b = 80;
+        int c = 70;
+        int d = 60;
+        string letter;
+        // int f;
+
+        // Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is Your grade percentage?");
+        string userGradeInput = Console.ReadLine();
+        int userScore = int.Parse(userGradeInput);
+
+        if (userScore >= a)
+        {
+            letter = "A";
+        }
+        else if (userScore >= b)
+        {
+            letter = "B";
+        }
+        else if (userScore >= c)
+        {
+            letter = "C";
+        }
+        else if (userScore >= d)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F Please take the course again";
+        }
+            Console.WriteLine($"Your grade is = {letter}.");
     }
 }
