f8c7d3dbe03d36c76851e384274fe7e52f788505
Nr1cha <vrm.richards@gmail.com>
2023-01-11 21:37:36 -0700

    added assignment for prep5. . .finally

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,47 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+
+    static String PromptUserName()
+    {
+        Console.Write("What is your name? ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+
+    static int PromptUserNumber()
+    {
+        Console.Write("what is your favorite number?");
+        int userFavNum = int.Parse(Console.ReadLine());
+        return userFavNum;
+    }
+
+
+    static int SquareNumber(int num)
+    {
+        int newNum = num * num;
+        return newNum;
+    }
+
+    static void DisplayResult(string userName, int userFavNum)
+    {
+        Console.Write($"{userName}, the square of your number is {userFavNum}");
+
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string usersName = PromptUserName();
+        int usersNum = PromptUserNumber();
+        int squaredNum = SquareNumber(usersNum);
+        DisplayResult(usersName, squaredNum); 
+
     }
 }
