8d1fa604fd71dd10037d388db2a283e16b099d0d
Nr1cha <vrm.richards@gmail.com>
2023-02-01 13:37:39 -0700

    finishing up getters and setters

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -17,10 +17,11 @@ class Program
         Console.WriteLine($"3rd constructor, passing 3 and 4: {f3.GetFractionString()}");
         Console.WriteLine($"3rd constructor, passing 3 and 4: {f3.GetDecimalValue()}");
 
-        Fraction f4 = new Fraction(1, 3);
-        Console.WriteLine($"3rd constructor, passing 1 and 3: {f4.GetFractionString()}");
-        Console.WriteLine($"3rd constructor, passing 1 and 3: {f4.GetDecimalValue()}");
+        Console.WriteLine("below is the getters and setters portion");
 
-        
+        f3.SetTop(1);
+        f3.SetBottom(3);
+        Console.WriteLine($"3rd constructor, passing 1 and 3: {f3.GetFractionString()}");
+        Console.WriteLine($"3rd constructor, passing 1 and 3: {f3.GetDecimalValue()}");
     }
 }
