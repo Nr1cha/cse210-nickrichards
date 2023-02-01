6f780ae8546f60f6650dba870584ca00e84cd9f5
Nr1cha <vrm.richards@gmail.com>
2023-02-01 10:11:36 -0700

    added methods

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -1,26 +1,39 @@
 using System;
 
-class Fraction
+public class Fraction
 {
     // variables / attributes
     private int _topNum;
     private int _bottomNum;
 
 
-    private Fraction()
+    public Fraction()
     {
         _topNum = 1;
         _bottomNum = 1;
     }
 
-    private Fraction(int wholeNumber)
+    public Fraction(int wholeNumber)
     {
         _topNum = wholeNumber;
+        _bottomNum = 1;
     }
 
-    private Fraction(int top, int bottom)
+    public Fraction(int top, int bottom)
     {
         _topNum = top;
         _bottomNum = bottom;
     }
+
+    public string GetFractionString()
+    {
+        string text = $"{_topNum}/{_bottomNum}";
+        return text;
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_topNum / (double)_bottomNum;
+    }
+
 }
