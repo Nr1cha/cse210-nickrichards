d5ff29f8878280a3f49636f0cbd13e3961ffdebd
Nr1cha <vrm.richards@gmail.com>
2023-02-06 13:53:03 -0700

    working on scripture class

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -6,15 +6,20 @@ class Program
     static void Main(string[] args)
     {
         // variables 
-        string refScript = "Alma 37: 37";
-        string hardScripture = "Counsel with the Lord in all thy doings, and he will direct thee for good; yea, when thou liest down at night lie down unto the Lord, that he may watch over you in your sleep; and when thou risest in the morning let thy heart be full of thanks unto God; and if ye do these things, ye shall be lifted up at the last day. ";
+        string staticReference = "Alma 37: 37";
+        string staticScripture = "Counsel with the Lord in all thy doings, and he will direct thee for good; yea, when thou liest down at night lie down unto the Lord, that he may watch over you in your sleep; and when thou risest in the morning let thy heart be full of thanks unto God; and if ye do these things, ye shall be lifted up at the last day. ";
+        Scripture thing = new Scripture(staticScripture); // invoking the scripture class to take staticScripture
         // OUTPUT
         Console.Clear();
-        Console.WriteLine($"{refScript}");
-        Console.WriteLine($"{hardScripture} \n");
-        Console.WriteLine("Press enter to continue or type 'quit' to finish:");
+        Console.WriteLine($"{staticReference}");
+        Console.WriteLine($"{staticScripture} \n");
+        Console.WriteLine("Press enter to continue or type 'quit' to finish: \n");
+
+        Console.Write(string.Join(" ", thing));
+
+
         // USER INPUT FOR TYPING TO THE CONSOLE
-        string userInput = Console.ReadLine();;
+        string userInput = Console.ReadLine();
         if (userInput == "quit")
         {
             Console.WriteLine("thanks for playing");
--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -2,6 +2,26 @@ using System;
 
 class Scripture
 {
-    // private string _scriptureText = List<word>
+    private List<string> stringThing = new List<string>();
+    
+
+
+    public Scripture(string stringThing) // CONSTRUCTOR
+    {
+        string[] wordArray = stringThing.Split(' ');
+        List<string> wordList = wordArray.ToList(); //turning it into a list of strings
+        this.stringThing = wordList;
+    }
+
+    // MAKE GETTERS AND SETTERS
+    public List<string> GetStrings() //GET
+    {
+        return stringThing;
+    }
+
+    public void SetScripture(List<string> pString) //SET
+    {
+        stringThing = pString;
+    }
 
 }
