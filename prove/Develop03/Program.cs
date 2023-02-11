594b098c7234bcbb6b7f2329d3ad1cef78721f35
Nr1cha <vrm.richards@gmail.com>
2023-02-11 11:50:47 -0700

    basic program asking user for input

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -7,12 +7,38 @@ class Program
     {
         // variables 
         string staticScripture = "Counsel with the Lord in all thy doings, and he will direct thee for good; yea, when thou liest down at night lie down unto the Lord, that he may watch over you in your sleep; and when thou risest in the morning let thy heart be full of thanks unto God; and if ye do these things, ye shall be lifted up at the last day. ";
+        string userInputBookName;
+        int userInputChaptNum;
+        int userInputVerseStartNum;
+        int userInputVerseEndNum;
+        string userInputScripture;
+
         Reference displayRef = new Reference("Alma", 37, 37, 89);
         Scripture currentScripture = new Scripture(displayRef, staticScripture); // invoking the scripture class to take staticScripture
 
         // OUTPUT
+        // todo get user input
+        Console.Write("Welcome to the Scripture Memorizor program!");
+        Console.WriteLine("Please type in the name of a scripture book. e.g. 'alma, nephi, etc..'");
+        userInputBookName = Console.ReadLine();
+
+        Console.WriteLine("Please type in a single chapter number:");
+        userInputChaptNum = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Please provide the start verse number:");
+        userInputVerseStartNum = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Now provide the ending verse number:");
+        userInputVerseEndNum = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Lastly, type out your scripture verse then press '[ENTER]'");
+        userInputScripture = Console.ReadLine();
+
+
+
+
+
         Console.Clear();
-        // Console.WriteLine($"{displayRef.DisplayReference()}");
         currentScripture.DisplayScripture();
 
 
@@ -36,7 +62,6 @@ class Program
             {
                 currentScripture.RemoveRandomWords(5); // remove a word when ENTER is pressed
                 Console.Clear();
-                // Console.WriteLine($"{displayRef.DisplayReference()}");
                 currentScripture.DisplayScripture(); // this shows the scripture
             }
         } while (userInput != "quit");
