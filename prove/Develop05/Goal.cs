db66bd9ab8c1f1621a6e23e901f0ae727e56aa84
Nr1cha <vrm.richards@gmail.com>
2023-03-06 19:13:52 -0700

    adding more content

--- prove/Develop05/Goal.cs
+++ prove/Develop05/Goal.cs
@@ -1,4 +1,15 @@
 public class Goal
 {
+    private string _name;
+    private string _description;
+    private int _goalPoints; 
 
+
+public string Goal(string name, string description, int points)
+{
+    return
+    _name = name;
+    _description = description;
+    _goalPoints = points;
+}
 }
--- prove/Develop05/ManageGoals.cs
+++ prove/Develop05/ManageGoals.cs
@@ -1,4 +1,32 @@
 public class ManageGoals
 {
+    List<Goal> _goalList = new List<Goal>();
 
+    private int _points;
+
+
+    public void SaveAllGoals()
+    {
+
+    }
+    public void LoadGoals()
+    {
+
+    }
+
+    public void AddGoalsToList()
+    {
+        
+    }
+
+    public int DisplayPoints(int points)
+    {
+        return _points = points;
+        
+    }
+
+    public void DisplayGoals()
+    {
+        
+    }
 }
