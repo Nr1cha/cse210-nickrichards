0afe18317da5311227b29ed3f24fdc31ad8e9cea
Nr1cha <vrm.richards@gmail.com>
2023-03-07 08:43:08 -0700

    adding things to classes

--- prove/Develop05/CheckListGoal.cs
+++ prove/Develop05/CheckListGoal.cs
@@ -1,4 +1,19 @@
 public class CheckListGoal
 {
+    private bool isComplete;
+    private int _numOfTimesRequired;
+    private int _numOfTimesCompleted;
+    private int _bonus;
+
+    public void RecordEvent();
+    public bool IsComplete()
+    {
+
+    }
+
+    public override int CalculatePoints()
+    {
+
+    }
 
 }
