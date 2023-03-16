affa2b030ccbc69e423b48646dd674a0c4ffe606
Nr1cha <vrm.richards@gmail.com>
2023-03-16 11:37:25 -0600

    formatting

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -7,19 +7,19 @@ class Program
         int userInput; //number input from user
         do
         {
-            Console.WriteLine("Hello FinalProject World!");
-            Console.WriteLine("");
+            Console.Clear();
             Console.WriteLine("Welcome to the DND character information screen.");
-            Console.WriteLine("Please select a class to view a character for information about them");
-            Console.WriteLine("1. Barbarian");
-            Console.WriteLine("2. Bard");
-            Console.WriteLine("3. Cleric");
-            Console.WriteLine("4. Druid");
-            Console.WriteLine("5. Monk");
-            Console.WriteLine("6. Ranger");
-            Console.WriteLine("7. Warlock");
-            Console.WriteLine("8. Wizard");
-            Console.WriteLine("9. Exit");
+            Console.WriteLine("Please select a class to view information about a character.\n");
+            Console.WriteLine("     1. Barbarian");
+            Console.WriteLine("     2. Bard");
+            Console.WriteLine("     3. Cleric");
+            Console.WriteLine("     4. Druid");
+            Console.WriteLine("     5. Monk");
+            Console.WriteLine("     6. Ranger");
+            Console.WriteLine("     7. Warlock");
+            Console.WriteLine("     8. Wizard");
+            Console.WriteLine("     9. Exit\n");
+            Console.Write("> Please make a selection: ");
             userInput = int.Parse(Console.ReadLine());
         }
         while (userInput != 9);
